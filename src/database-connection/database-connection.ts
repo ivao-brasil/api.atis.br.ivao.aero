52c605b711291b15208ac685cf4af6eaea5c20b8
Bruno Ricci <9812550+brunorrr@users.noreply.github.com>
2023-08-04 21:20:09 -0300

    feat: add database varibles

--- src/database-connection/database-connection.ts
+++ src/database-connection/database-connection.ts
@@ -6,11 +6,11 @@ export const databaseProviders = [
     useFactory: async () => {
       const dataSource = new DataSource({
         type: 'mysql',
-        host: 'localhost',
-        port: 3306,
-        username: 'root',
-        password: 'root',
-        database: 'brivao_atis',
+        host: process.env.DATABASE_HOST || 'localhost',
+        port: +(process.env.DATABASE_PORT || 3306),
+        username: process.env.DATABASE_USERNAME || 'root',
+        password: process.env.DATABASE_PASSWORD || 'root',
+        database: process.env.DATABASE_SCHEMA || 'brivao_atis',
         entities: [
             __dirname + '/../**/*.entity{.ts,.js}',
         ],
